d54df60b9ef46bd63f91f96adf7ef4bf0059c2f4
Destiny Frith <desilyvahn@gmail.com>
2022-02-07 15:36:42 -0600

    storing custom orders and changing permanent state

--- scripts/database.js
+++ scripts/database.js
@@ -33,10 +33,10 @@ const database = {
             styleId: 3,
             timestamp: 1614659931693
         }
-    ]
+    ],
+
     orderBuilder: {
-        
-    }
+    },
 }
 
 // export all sets of data to be used in other modules
@@ -57,7 +57,40 @@ export const getMetals = () => {
 }
 
 // export custom orders data
-// this is a simplified way to write map export function
 export const getOrders = () => {
-    return [...database.orders]
+    return database.customOrders.map(order => ({ ...order }))
+}
+
+// this is exporting the function setMetal so we can use it elsewhere
+export const setMetal = (id) => {
+    database.orderBuilder.metalId = id
+}
+
+export const setSize = (id) => {
+    database.orderBuilder.sizeId = id
+}
+
+export const setStyle = (id) => {
+    database.orderBuilder.styleId = id
+}
+
+export const addCustomOrder = () => {
+    // Copy the current state of user choices
+    const newOrder = { ...database.orderBuilder }
+
+    // Add a new primary key to the object
+    const lastIndex = database.customOrders.length - 1
+    newOrder.id = database.customOrders[lastIndex].id + 1
+
+    // Add a timestamp to the order
+    newOrder.timestamp = Date.now()
+
+    // Add the new order object to custom orders state
+    database.customOrders.push(newOrder)
+
+    // Reset the temporary state for user choices
+    database.orderBuilder = {}
+
+    // Broadcast a notification that permanent state has changed
+    document.dispatchEvent(new CustomEvent("stateChanged"))
 }
